cc75600efebd5877c01e37fb62ce7ac9b225892c
Steven Degutis <steven.degutis@gmail.com>
2012-03-09 17:34:07 -0600

    preparing to test

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
 LDFLAGS = -framework CoreServices -framework Cocoa
 
-fswatch: main.o
+fswatch: fswatch.o main.o
 	$(CC) $(LDFLAGS) $^ -o $@
 
+testrunner: fswatch.o test.o
+	$(CC) $(LDFLAGS) $^ -o $@
+
+test: testrunner
+	@./testrunner
+	@printf "\e[1;32mPASS\e[0m\n"
+
 clean:
-	rm main.o fswatch
+	rm -f fswatch.o main.o test.o fswatch testrunner
--- main.m
+++ main.m
@@ -1,14 +1,5 @@
-//
-//  main.c
-//  fswatch
-//
-//  Created by Steven Degutis on 3/9/12.
-//  Copyright 2012 8th Light. All rights reserved.
-//
-
 #include <stdio.h>
-#include <CoreServices/CoreServices.h>
-
+#import <CoreServices/CoreServices.h>
 #import <Foundation/Foundation.h>
 
 NSString* watchfulCommandToRun;
@@ -22,37 +13,37 @@ int main (int argc, char * argv[]) {
     argc = 4;
     argv = (char*[]){ "me", "pwd", "/Users/sdegutis/projects/go/src/github.com/sdegutis/mapstruct", "/Users/sdegutis/projects/go/src/github.com/sdegutis/blog" };
 
-    
+
     [NSAutoreleasePool new];
-    
+
     if (argc < 2) {
         printf("usage: %s command path ...", argv[0]);
         exit(1);
     }
-    
+
     watchfulCommandToRun = [[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] retain];
-    
+
     CFMutableArrayRef pathsToWatch = CFArrayCreateMutable(NULL, 0, &kCFTypeArrayCallBacks);
-    
+
     for (int i = 2; i < argc; i++) {
         CFArrayAppendValue(pathsToWatch, CFStringCreateWithCString(NULL, argv[i], kCFStringEncodingUTF8));
     }
-    
+
     CFShow(pathsToWatch);
-    
-    FSEventStreamRef stream = FSEventStreamCreate(NULL, callback, NULL, pathsToWatch, kFSEventStreamEventIdSinceNow, 0.5, kFSEventStreamCreateFlagFileEvents);
+
+    FSEventStreamRef stream = FSEventStreamCreate(NULL, callback, NULL, pathsToWatch, kFSEventStreamEventIdSinceNow, 0, kFSEventStreamCreateFlagFileEvents);
     FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopCommonModes);
     if (!FSEventStreamStart(stream)) {
         fprintf(stderr, "error: failed to run for some reason\n");
         exit(1);
     }
-    
+
     CFRunLoopRun();
-    
+
     // we NEVER get here. ever. period.
     FSEventStreamStop(stream);
     FSEventStreamInvalidate(stream);
     FSEventStreamRelease(stream);
-    
+
     return 0;
 }
