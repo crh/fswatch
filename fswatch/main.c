deafcdc3e35fb4baaac2a531628ac3302d37e8f7
Steven Degutis <steven.degutis@gmail.com>
2012-03-09 15:23:57 -0600

    Initial Commit

---
+++ fswatch/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  fswatch
+//
+//  Created by Steven Degutis on 3/9/12.
+//  Copyright 2012 8th Light. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
