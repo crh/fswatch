4fc1e278847d6f3b2936bb925981b430fa55e7ee
Steven Degutis <steven.degutis@gmail.com>
2012-03-10 09:29:24 -0600

    making it easier to install without me having forked homebrew

---
+++ brew_formula/fswatch.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Fswatch < Formula
+  homepage 'https://github.com/sdegutis/fswatch'
+
+  url 'https://github.com/sdegutis/fswatch/tarball/v2.0'
+  md5 '2c9b0bc0b6ade5cd3c302d5743c0ade1'
+
+  head 'https://github.com/sdegutis/fswatch.git'
+
+  def install
+    system "make"
+    bin.install "fswatch"
+  end
+end
