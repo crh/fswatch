a637431b8961b44cc8b04ae653af4be0bf5d9b47
Steven Degutis <steven.degutis@gmail.com>
2012-03-12 00:11:58 -0500

    update version kinda

--- brew_formula/fswatch.rb
+++ brew_formula/fswatch.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Fswatch < Formula
   homepage 'https://github.com/sdegutis/fswatch'
 
-  url 'https://github.com/sdegutis/fswatch/tarball/v2.0'
+  url 'https://github.com/sdegutis/fswatch/tarball/v2.1'
   md5 '2c9b0bc0b6ade5cd3c302d5743c0ade1'
 
   head 'https://github.com/sdegutis/fswatch.git'
