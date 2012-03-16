793ce05d5707136482d25084b3aeabb23df5ec9f
Steven Degutis <steven.degutis@gmail.com>
2012-03-16 08:22:48 -0500

    reset all attributes before printing the command

--- cmd_decorator.go
+++ cmd_decorator.go
@@ -5,7 +5,7 @@ import "strings"
 
 func decorate(cmd command, exec func(command)) {
   cmdStrings := append([]string{cmd.name}, cmd.args...)
-  fmt.Fprintf(cmd.outPipe, "%c[34;4m%s%c[0m\n", 27, strings.Join(cmdStrings, " "), 27)
+  fmt.Fprintf(cmd.outPipe, "%c[0;34;4m%s%c[0m\n", 27, strings.Join(cmdStrings, " "), 27)
   exec(cmd)
   fmt.Fprintln(cmd.outPipe)
 }
